20c5b91037d2bdcd3ee3327e8980d08608bb03fa
Afridisolo <mohamedafridi515@gmail.com>
2024-09-06 12:41:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import Body from './components/Body';
+import Brands from './components/Brands';
+import Read from './components/Read';
+import Final from './components/Final';
+import Footer from './components/Footer'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+function App(){
+  return(
+   <>
+  <Router>
+    <Header ></Header> 
+    <Routes>
+          <Route path="/product" element={<Body />}></Route>
+          <Route path="/drivemore" element={<Brands />}></Route>
+          <Route path="/readmore" element={<Read />}></Route>
+          <Route path="/unlock" element={<Final />}></Route>
+          <Route path="/footer" element={<Footer />}></Route>
+      </Routes>
+
+    </Router>
+   </>
+  );
+};
+export default App;
